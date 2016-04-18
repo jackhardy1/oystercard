d2c41364f7ff457be7f57a2bc59db16cf2ec2cc8
Jack Hardy <jackhardy1@gmail.com>
2016-04-18 17:21:55 +0100

    rspec for 'stores entry station'

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,18 +2,16 @@ class Oystercard
 attr_reader :balance
 MAXIMUM_LIMIT = 90
 MINIMUM_BALANCE = 1
+MINIMUM_FARE = 1
+
   def initialize
     @balance = 0
     @in_journey = false
   end
 
   def top_up(amount)
-    fail "The maximum limit is 90" if amount + @balance > MAXIMUM_LIMIT
+    fail "The maximum limit is #{MAXIMUM_LIMIT}" if amount + @balance > MAXIMUM_LIMIT
     @balance += amount
-  end
-
-  def deduct(amount)
-    @balance -= amount
   end
 
   def in_journey?
@@ -27,5 +25,11 @@ MINIMUM_BALANCE = 1
 
   def touch_out
     @in_journey = false
+    deduct(MINIMUM_FARE)
+  end
+
+  private
+  def deduct(amount)
+    @balance -= amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
+  let (:station) {double :station}
+
     it "has a default balance of 0" do
       expect(subject.balance).to eq(0)
     end
@@ -17,14 +19,7 @@ describe Oystercard do
       end
     end
 
-    describe "#deduct" do
-      it "deducts an amount from balance for a journey" do
-        subject.top_up(10)
-        expect{subject.deduct(5)}.to change{subject.balance}.by(-5)
-      end
-    end
-
-    describe "in_journey?" do
+    describe "#in_journey?" do
       it "starts outside of a journey" do
         expect(subject.in_journey?).to eq false
       end
@@ -40,8 +35,13 @@ describe Oystercard do
       it "it raises an error on 'touch_in', if balance is less than £1" do
         expect{subject.touch_in}.to raise_error "Insufficient funds"
       end
-    end
 
+      it "it stores the entry station" do
+        subject.top_up(1)
+        subject.touch_in(station)
+        expect(subject.entry_station).to eq station
+      end
+    end
 
     describe "#touch_out" do
       it "should change value of in_journey to be false" do
@@ -54,7 +54,12 @@ describe Oystercard do
       it "should deduct the minimum fare" do
         subject.top_up(5)
         subject.touch_in
-        expect(subject.touch_out).to change{subject.balance}.by(-Oystercard::MINIMUM_FARE)
+        expect{subject.touch_out}.to change{subject.balance}.by(-Oystercard::MINIMUM_FARE)
+      end
+
+      it "deducts an amount from balance for a journey" do
+        subject.top_up(10)
+        expect{subject.touch_out}.to change{subject.balance}.by(-1)
       end
     end
 end
