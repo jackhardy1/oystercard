4d8e5c46faa45ea7abd869375eac3e1306ab7ed6
Jack Hardy <jackhardy1@gmail.com>
2016-04-18 16:30:31 +0100

    Updated top_up method, and rspec for deduct

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 class Oystercard
 attr_reader :balance
+MAXIMUM_LIMIT = 90
 
-def initialize
-  @balance = 0
-end
+  def initialize
+    @balance = 0
+  end
 
-def top_up(amount)
-  @balance += amount
-end
-
+  def top_up(amount)
+    fail "The maximum limit is 90" if amount + @balance > MAXIMUM_LIMIT
+    @balance += amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,14 +5,23 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
 
-
     describe "#top_up" do
       it "increases the balance when the top_up method is called" do
         expect{subject.top_up(5)}.to change{subject.balance}.by 5
       end
 
       it "raises an error if amount + balance exceeds maximum limit" do
-        expect{subject.top_up(91)}.to raise_error("The maximum limit is 90")
+        max = Oystercard::MAXIMUM_LIMIT
+        subject.top_up(max)
+        expect{subject.top_up(1)}.to raise_error("The maximum limit is #{max}")
       end
     end
+
+    describe "#deduct" do
+      it "deducts an amount from balance for a journey" do
+        subject.top_up(10)
+        expect{subject.deduct(5)}.to change{subject.balance}.by -5
+      end
+    end
+
 end
