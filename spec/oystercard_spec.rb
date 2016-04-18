5c1e573ed6bf01e749612a1978e837bc0d3600c1
Jack Hardy <jackhardy1@gmail.com>
2016-04-18 16:01:19 +0100

    Created rspec for top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,12 @@ describe Oystercard do
     it "has a default balance of 0" do
       expect(subject.balance).to eq(0)
     end
+
+
+    describe "#top_up" do
+      it "increases the balance when the top_up method is called" do
+        subject.top_up(5)
+        expect(subject.balance).to eq(5)
+      end
+    end
 end
