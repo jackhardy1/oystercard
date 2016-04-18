91c98d9393ba156f7c7d2253c0de0db16f3fd741
Alex Robertson <lex1989@gmail.com>
2016-04-18 16:16:37 +0100

    Created max limit test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,8 +8,11 @@ describe Oystercard do
 
     describe "#top_up" do
       it "increases the balance when the top_up method is called" do
-        subject.top_up(5)
-        expect(subject.balance).to eq(5)
+        expect{subject.top_up(5)}.to change{subject.balance}.by 5
+      end
+
+      it "raises an error if amount + balance exceeds maximum limit" do
+        expect{subject.top_up(91)}.to raise_error("The maximum limit is 90")
       end
     end
 end
