ed94dd42c60dc924527dd627fe660fde77ec694d
Alex Robertson <lex1989@gmail.com>
2016-04-18 17:29:24 +0100

    Added touch_out nil entry_station test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-attr_reader :balance
+attr_reader :balance, :entry_station
 MAXIMUM_LIMIT = 90
 MINIMUM_BALANCE = 1
 MINIMUM_FARE = 1
@@ -7,6 +7,7 @@ MINIMUM_FARE = 1
   def initialize
     @balance = 0
     @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(amount)
@@ -18,8 +19,9 @@ MINIMUM_FARE = 1
     @in_journey
   end
 
-  def touch_in
+  def touch_in(station)
     fail "Insufficient funds" if @balance < MINIMUM_BALANCE
+    @entry_station = station
     @in_journey = true
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -28,12 +28,12 @@ describe Oystercard do
     describe "#touch_in" do
       it "should change value of in_journey to be true" do
         subject.top_up(5)
-        subject.touch_in
+        subject.touch_in(station)
         expect(subject.in_journey?).to eq true
       end
 
       it "it raises an error on 'touch_in', if balance is less than £1" do
-        expect{subject.touch_in}.to raise_error "Insufficient funds"
+        expect{subject.touch_in(station)}.to raise_error "Insufficient funds"
       end
 
       it "it stores the entry station" do
@@ -46,20 +46,27 @@ describe Oystercard do
     describe "#touch_out" do
       it "should change value of in_journey to be false" do
         subject.top_up(5)
-        subject.touch_in
+        subject.touch_in(station)
         subject.touch_out
         expect(subject.in_journey?).to eq false
       end
 
       it "should deduct the minimum fare" do
         subject.top_up(5)
-        subject.touch_in
+        subject.touch_in(station)
         expect{subject.touch_out}.to change{subject.balance}.by(-Oystercard::MINIMUM_FARE)
       end
 
       it "deducts an amount from balance for a journey" do
         subject.top_up(10)
         expect{subject.touch_out}.to change{subject.balance}.by(-1)
+      end
+
+      it "sets the entry station to nil" do
+        subject.top_up(5)
+        subject.touch_in(station)
+        subject.touch_out
+        expect(subject.entry_station).to eq nil
       end
     end
 end
