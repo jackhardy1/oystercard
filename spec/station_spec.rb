10e0e60530f67e2e880376bc3772c091b9672ba6
Noah Pollock <pollocknoah@gmail.com>
2016-04-19 17:59:51 +0100

    step 14 done

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,9 +3,16 @@ require 'station'
 describe Station do
 
   describe "initialize" do
-    it "starts with a name" do
+    it "sets a name" do
       holborn = Station.new('holborn',2)
       expect(holborn.name).to eq('holborn')
     end
+
+    it "sets the zone" do
+      holborn = Station.new('holborn',2)
+      expect(holborn.zone).to eq(2)
+    end
+
   end
+
 end
