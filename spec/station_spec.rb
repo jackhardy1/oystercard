6b3782ea27afaeadbf892c5a36bccd22b1aec33b
Jack Hardy <jackhardy1@gmail.com>
2016-04-19 17:14:43 +0100

    Station expects name

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,5 +2,10 @@ require 'station'
 
 describe Station do
 
-  
+  describe "initialize" do
+    it "starts with a name" do
+      holborn = Station.new('holborn',2)
+      expect(holborn.name).to eq('holborn')
+    end
+  end
 end
