83538c250bb188f552f993b77d0b7b38a439e40c
Jack Hardy <jackhardy1@gmail.com>
2016-04-19 11:36:39 +0100

    Rspec top_up method test added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,8 @@
 #Test for day two
 class Oystercard
+  attr_reader :balance
 
+  def initialize
+    @balance = 0
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,15 @@ require 'oystercard'
 #Test for day two
 describe Oystercard do
 
-  it " has default balance of 0" do
-    expect(subject).to eq(0)
+  it "has default balance of 0" do
+    expect(subject.balance).to eq(0)
   end
-  
+
+  describe "#top_up" do
+
+    it "tops up balance" do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+  end
 end
